636a1199f671318b328a7791ef350277bfd3b1a3
pa-feketeb <112852275+pa-feketeb@users.noreply.github.com>
2023-02-23 07:41:51 +0100

    Create feladat_3.py

---
+++ feladat_3.py
@@ -0,0 +1,10 @@
+#Fekete Balázs
+#10D 1.Csoport
+
+
+honapok = ["január","február","március","április","május","június"]
+
+index = 0
+for honap in honapok:
+    print(index, honap)
+    index = index +1
